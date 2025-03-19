adbacb0feed50729fbe9c3000776814c36c70db3
Greta Simonian <greta77762@gmail.com>
2025-03-19 13:01:50 -0700

    add swagLabTest

--- src/test/java/school/redrover/Group_JavaQATest.java
+++ src/test/java/school/redrover/Group_JavaQATest.java
@@ -156,4 +156,29 @@ public class Group_JavaQATest {
     }
 
 
+    @Test
+    public void swagLabTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.xpath("//input[@id = 'user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//form/input[@id = 'login-button']")).click();
+        driver.findElement(By.xpath("//button[@id = 'add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.id("shopping_cart_container")).click();
+        driver.findElement(By.xpath("//button[@id = 'checkout']" )).click();
+        driver.findElement(By.xpath("//input[@id = 'first-name']")).sendKeys("Anna");
+        driver.findElement(By.xpath("//input[@id = 'last-name']")).sendKeys("Anina");
+        driver.findElement(By.xpath("//input[@id = 'postal-code']")).sendKeys("19283");
+        driver.findElement(By.xpath("//input[@id = 'continue'] ")).click();
+        driver.findElement(By.xpath("//button[@id= 'finish']")).click();
+        WebElement confirm = driver.findElement(By.xpath("//div/div/h2"));
+        String confirmText = confirm.getText();
+        assertEquals(confirmText, "Thank you for your order!");
+        driver.findElement(By.xpath("//button[@id= 'back-to-products']")).click();
+
+        driver.quit();
+
+    }
 }
