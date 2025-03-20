24a07ca8e66f5dc0c1af053e11b106336df88ae0
CargoLeaderDenis <denistrofim@yahoo.com>
2025-03-20 19:30:16 -0400

    draft

---
+++ src/test/java/school/redrover/DtTest.java
@@ -0,0 +1,26 @@
+package school.redrover;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DtTest {
+
+    @Test
+    public void testA() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get ("khl.ru");
+
+        WebElement searchButton = driver.findElement()
+        searchButton.click();
+
+        WebElement searchButton = driver.findElement()
+        searchButton.click();
+
+        Assert.assertEquals();
+        driver.quit();
+    }
+
+}
