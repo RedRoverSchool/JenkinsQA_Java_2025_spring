e92dc57fac64fc6e98b2ef5f0b47aecba13dc0f7
Yuliya Bozheva <yulka818818@gmail>
2025-03-20 11:30:51 -0300

    add сhandghesSecond_Test

--- src/test/java/school/redrover/GroupAutoamigosTest.java
+++ src/test/java/school/redrover/GroupAutoamigosTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,40 +12,32 @@ public class GroupAutoamigosTest {
     private WebDriver driver;
 
     @BeforeMethod
-      public void setUp() {
+    public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
     }
 
     @Test
-    public void testWebFormLink() throws InterruptedException {
-        checkLink("Web form");
-    }
-
-    @Test
-    public void testNavigationLink() throws InterruptedException {
-        checkLink("Navigation");
-    }
-
-    @Test
-    public void testDropdownMenuLink() throws InterruptedException {
-        checkLink("Dropdown menu");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-
-    // Метод для клика по ссылке и проверки, что страница открылась
-    private void checkLink(String linkText) throws InterruptedException {
-        WebElement link = driver.findElement(By.linkText(linkText));
+    public void testWebFormLink() {
+        WebElement link = driver.findElement(By.linkText("Web form"));
         link.click();
+        Assert.assertNotEquals(driver.getTitle(), "Страница не загрузилась: Web form");
+        driver.navigate().back();
+    }
 
-        Thread.sleep(2000);
+    @Test
+    public void testNavigationLink() {
+        WebElement link = driver.findElement(By.linkText("Navigation"));
+        link.click();
+        Assert.assertNotEquals(driver.getTitle(), "Страница не загрузилась: Navigation");
+        driver.navigate().back();
+    }
 
-        Assert.assertNotEquals(driver.getTitle() + "", "Страница не загрузилась: " + linkText);
-
+    @Test
+    public void testDropdownMenuLink() {
+        WebElement link = driver.findElement(By.linkText("Dropdown menu"));
+        link.click();
+        Assert.assertNotEquals(driver.getTitle(), "Страница не загрузилась: Dropdown menu");
         driver.navigate().back();
     }
 }
